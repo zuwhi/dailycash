7e12346d0f3df6a71f0923ac150966a25283e0f9
zuwhi <zuwhyyy@gmail.com>
2025-06-20 00:18:15 +0700

    omke2

--- src/app/dashboard/cash/page.tsx
+++ src/app/dashboard/cash/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+"use server";
 
 import React, { useEffect, useState } from "react";
 import {
--- src/app/dashboard/category/page.tsx
+++ src/app/dashboard/category/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+
 import React, { useEffect, useState } from "react";
 import {
   databases,
